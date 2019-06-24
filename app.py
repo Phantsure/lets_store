d3712ef8f530e844a7783418c7dab1a3c1d9049a
Phantsure <samparksharma2000@gmail.com>
2019-06-24 20:08:12 +0530

    support for directory upload and update added

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ def main():
         print("use --help to get help")
     elif sys.argv[1] == '--upload':
         if l == 3:
-            upload(sys.argv[2])
+            f = []
+            f.append(sys.argv[2].split('/')[-1])
+            upload(sys.argv[2][:-(len(f[0]) + 1)], f)
+            # print(1)
     elif sys.argv[1] == '--update':
         if l == 3:
-            update(sys.argv[2])
+            f = []
+            f.append(sys.argv[2].split('/')[-1])
+            update(sys.argv[2][:-(len(f[0]) + 1)], f)
     elif sys.argv[1] == '--delete':
         if l == 3:
             delete(sys.argv[2])
@@ -32,49 +37,74 @@ def main():
 # https://gitlab.com/api/v4/projects/<:id>/repository/files/<filename-with-extension>
 # <:id> is project id, which can be found in the settings of your gitlab repo
 
-def upload(file):
-    # with open(file, encoding='utf-8', mode='r') as f:
-    with open(file, mode='rb+') as f:
-        name = f.name.split('/')[-1]
-        # print(name)
-        # content = "{}".format(f.read())
-        content = "{}".format(base64.b64encode(f.read()).decode())
-        commit_message = "Uploaded on {}".format(ctime())
-        # print(content)
-        # print(content.encode())
-        headers = {'Private-Token': access_token, 'Content-Type': 'application/json'}
-        data = {"branch": "master","content": content, "commit_message": commit_message}
-        # print(data.encode())
-        # files/<folder-name>%2F
-        r = requests.post("https://gitlab.com/api/v4/projects/"+ project_id +"/repository/files/" + name, headers=headers, json=data)
+import os
+import os.path
 
-    if r.status_code >= 400:
-        print("Error {}".format(r.status_code))
-    else:
-        print("Success with status code:{}".format(r.status_code))
+# def list_files_from_directory(directory):
+#     return os.listdir(directory)
+
+def upload(directory, files, level=1):
+    print(directory + '/' + files[0])
+    print(files)
+    for file in files:
+        if os.path.isfile(directory + '/' + file):
+            print(2)
+            # with open(file, encoding='utf-8', mode='r') as f:
+            with open(directory + '/' + file, mode='rb+') as f:
+                name = f.name.split('/')[-level:]
+                while(len(name) != 1):
+                    name[-2] = name[-2] + '%2F' + name[-1]
+                    del(name[-1])
+                # print(name)
+                # content = "{}".format(f.read())
+                content = "{}".format(base64.b64encode(f.read()).decode())
+                commit_message = "Uploaded on {}".format(ctime())
+                # print(content)
+                # print(content.encode())
+                headers = {'Private-Token': access_token, 'Content-Type': 'application/json'}
+                data = {"branch": "master","content": content, "commit_message": commit_message}
+                # print(data.encode())
+                # files/<folder-name>%2F
+                r = requests.post("https://gitlab.com/api/v4/projects/"+ project_id +"/repository/files/" + name[0], headers=headers, json=data)
+
+            if r.status_code >= 400:
+                print("Error {}".format(r.status_code))
+            else:
+                print("Success with status code:{}".format(r.status_code))
+        elif os.path.isdir(directory + '/' + file):
+            upload(directory+'/'+file, os.listdir(directory + '/' + file), level=level+1)
     return
 
-def update(file):
-    # with open(file, encoding='utf-8', mode='r') as f:
-    with open(file, mode='rb+') as f:
-        name = f.name.split('/')[-1]
-        # print(name)
-        content = "{}".format(base64.b64encode(f.read()).decode())
-        commit_message = "Updated on {}".format(ctime())
-        # print(content)
-        # print(content.encode())
-        headers = {'Private-Token': access_token, 'Content-Type': 'application/json'}
-        data = {"branch": "master","content": content, "commit_message": commit_message}
-        # print(data.encode())
-        # a = Request("https://gitlab.com/api/v4/projects/"+ project_id +"/repository/files/" + name, data=data.encode(), headers={"Private-Token": access_token, "Content-Type": "application/json"}, method="PUT")
-        # urlopen(a)
-        r = requests.put("https://gitlab.com/api/v4/projects/"+ project_id +"/repository/files/" + name, headers=headers, json=data)
-        print("updated")
+def update(directory, files, level=1):
+    print(directory + '/' + files[0])
+    print(files)
+    for file in files:
+        if os.path.isfile(directory + '/' + file):
+            print(2)
+            # with open(file, encoding='utf-8', mode='r') as f:
+            with open(directory + '/' + file, mode='rb+') as f:
+                name = f.name.split('/')[-level:]
+                while(len(name) != 1):
+                    name[-2] = name[-2] + '%2F' + name[-1]
+                    del(name[-1])
+                # print(name)
+                # content = "{}".format(f.read())
+                content = "{}".format(base64.b64encode(f.read()).decode())
+                commit_message = "Updated on {}".format(ctime())
+                # print(content)
+                # print(content.encode())
+                headers = {'Private-Token': access_token, 'Content-Type': 'application/json'}
+                data = {"branch": "master","content": content, "commit_message": commit_message}
+                # print(data.encode())
+                # files/<folder-name>%2F
+                r = requests.put("https://gitlab.com/api/v4/projects/"+ project_id +"/repository/files/" + name[0], headers=headers, json=data)
 
-    if r.status_code >= 400:
-        print("Error {}".format(r.status_code))
-    else:
-        print("Success with status code:{}".format(r.status_code))
+            if r.status_code >= 400:
+                print("Error {}".format(r.status_code))
+            else:
+                print("Success with status code:{}".format(r.status_code))
+        elif os.path.isdir(directory + '/' + file):
+            update(directory+'/'+file, os.listdir(directory + '/' + file), level=level+1)
     return
 
 def delete(file):
