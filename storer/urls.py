a63ebe15c590f6b06785ef4ee45b4cdf072dc23b
Phantsure <samparksharma2000@gmail.com>
2019-07-28 04:15:26 +0530

    ui update

--- storer/urls.py
+++ storer/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'storer'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('files/', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
     path('update/', views.update, name='update'),
     path('delete/<path:filename>', views.delete, name='delete'),
     path('download/<path:filename>', views.download, name='download'),
-    path('files/', views.files, name='files'),
+    path('', views.files, name='files'),
     path('details/<path:filename>', views.file_details, name='details'),
 ]
